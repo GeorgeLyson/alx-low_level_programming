8d31879e1d7991cc6ff722b7bfbc71a8c2b8e5ce
GeorgeLyson <georgelysonglo@gmail.com>
2022-09-10 13:54:15 -0700

    Four

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - print the string in the put fuction
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a miltilingual puzzle");
+	return(0);
+}
