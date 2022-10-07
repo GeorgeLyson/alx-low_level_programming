177b9684eb4dcc98d5ffaf2f8cb133bbc96b5a41
GeorgeLyson <111042846+GeorgeLyson@users.noreply.github.com>
2022-10-07 16:46:57 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,16 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - allocates memory using malloc
- *
- * @b: amount of bytes
- *
- * Return: pointer to our new allocated memory
- *         exit with 98 if malloc fails
-*/
+ * *malloc_checked - allocates memory using malloc and exit if failed
+ * @b: int
+ * Return: pointer to the array initialized or NULL
+ */
 
 void *malloc_checked(unsigned int b)
 {
-	void *new_mem;
+int *m = malloc(b);
+if (m == 0)
+	exit(98);
 
-	new_mem = malloc(b);
-	if (new_mem == NULL)
-		exit(98);
-	return (new_mem);
+return (m);
 }
